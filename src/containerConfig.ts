477dd931d6e3fc2e6f2e6aa6c3c104a008f7f414
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2020-12-27 09:51:44 +0200

    feat(configurations): added healthcheck and shutdown

--- src/containerConfig.ts
+++ src/containerConfig.ts
@@ -7,6 +7,23 @@ import { MCLogger, ILoggerConfig, IServiceConfig } from '@map-colonies/mc-logger
 import { Services } from './common/constants';
 import { Entity } from './entity/models/entity';
 
+const healthCheck = (connection: Connection): (() => Promise<void>) => {
+  return async (): Promise<void> => {
+    try {
+      await connection.query('SELECT 1')
+    } catch {
+      throw new Error("db not responding :(");
+    }
+    return Promise.resolve();
+  };
+};
+
+const beforeShutdown = (connection: Connection): (() => Promise<void>) => {
+  return async (): Promise<void> => {
+    await connection.close();
+  };
+};
+
 async function registerExternalValues(): Promise<void> {
   const loggerConfig = config.get<ILoggerConfig>('logger');
   const packageContent = readFileSync('./package.json', 'utf8');
@@ -16,11 +33,14 @@ async function registerExternalValues(): Promise<void> {
   container.register(Services.LOGGER, { useValue: logger });
 
   const connectionOptions = config.get<ConnectionOptions>('db');
-  const connection = await createConnection({entities: ['entity/models/*.js'] ,...connectionOptions});
-  container.register(Connection, { useValue: connection});
-  container.register('EntityRepository', {useValue: connection.getRepository(Entity)})
+  const connection = await createConnection({ entities: ['entity/models/*.js'], ...connectionOptions });
+  container.register(Connection, { useValue: connection });
+  container.register('EntityRepository', { useValue: connection.getRepository(Entity) });
 
-  container.register<Probe>(Probe, { useFactory: (container) => new Probe(container.resolve(Services.LOGGER), {}) });
+  container.register<Probe>(Probe, {
+    useFactory: (container) =>
+      new Probe(container.resolve(Services.LOGGER), { liveness: healthCheck(connection), beforeShutdown: beforeShutdown(connection) }),
+  });
 }
 
 export { registerExternalValues };
