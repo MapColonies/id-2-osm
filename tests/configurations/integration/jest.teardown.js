57c83ab0b338953ec634302afebef6c58d3a1039
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2020-12-29 10:22:31 +0200

    fix(configurations): fixes based on the pr

--- tests/configurations/integration/jest.teardown.js
+++ tests/configurations/integration/jest.teardown.js
@@ -3,7 +3,6 @@ const fs = require('fs').promises;
 const glob = require('glob');
 
 module.exports = async () => {
-  // await fs.unlink(config.get('db.database'))
   glob.Glob(config.get('db.database') + '*', {}, async (err, files) => {
     await Promise.all(files.map((file) => fs.unlink(file)));
   });
