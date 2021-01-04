f3c10395a4fb3c7de30325326d89acd90999a1ed
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2021-01-04 10:49:58 +0200

    test(configurations): added a merged report

--- tests/configurations/integration/jest.config.js
+++ tests/configurations/integration/jest.config.js
@@ -7,6 +7,10 @@ module.exports = {
   testMatch: ['<rootDir>/tests/integration/**/*.spec.ts'],
   setupFiles: ['<rootDir>/tests/configurations/jest.setup.js'],
   globalTeardown: '<rootDir>/tests/configurations/integration/jest.teardown.js',
+  reporters: [
+    'default',
+    ['jest-html-reporters', { multipleReportsUnitePath: './reports', pageTitle: 'integration', publicPath: './reports', filename: 'integration.html' }],
+  ],
   collectCoverage: true,
   moduleDirectories: ['node_modules', 'src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts}', '!**/node_modules/**', '!**/vendor/**'],
