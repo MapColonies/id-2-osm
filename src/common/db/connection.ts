da6dc051d284cab1ffeac8a567f28eb5b27167bb
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2021-03-24 13:50:08 +0200

    feat(configurations): enabled cert auth on migrations (#43)

--- src/common/db/connection.ts
+++ src/common/db/connection.ts
@@ -1,12 +1,16 @@
 import { readFileSync } from 'fs';
-import { Connection, createConnection } from 'typeorm';
+import { Connection, ConnectionOptions, createConnection } from 'typeorm';
 import { DbConfig } from '../interfaces';
 
-export const initConnection = async (dbConfig: DbConfig): Promise<Connection> => {
+export const createConnectionOptions = (dbConfig: DbConfig): ConnectionOptions => {
   const { enableSslAuth, sslPaths, ...connectionOptions } = dbConfig;
   if (enableSslAuth && connectionOptions.type === 'postgres') {
     connectionOptions.password = undefined;
     connectionOptions.ssl = { key: readFileSync(sslPaths.key), cert: readFileSync(sslPaths.cert), ca: readFileSync(sslPaths.ca) };
   }
-  return createConnection(connectionOptions);
+  return connectionOptions;
+};
+
+export const initConnection = async (dbConfig: DbConfig): Promise<Connection> => {
+  return createConnection(createConnectionOptions(dbConfig));
 };
