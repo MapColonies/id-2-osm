05165214d3d99df72fa466821d3a1553ab1d89a3
galta95 <48960890+galta95@users.noreply.github.com>
2021-07-01 10:36:59 +0300

    feat: added traceparent header on response (#73)

--- src/common/tracing.ts
+++ src/common/tracing.ts
@@ -4,7 +4,7 @@ import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { PgInstrumentation } from '@opentelemetry/instrumentation-pg';
 import { IGNORED_INCOMING_TRACE_ROUTES, IGNORED_OUTGOING_TRACE_ROUTES } from './constants';
 
-export const tracing = new Tracing('osm-change-generator', [
+export const tracing = new Tracing([
   new HttpInstrumentation({
     ignoreIncomingPaths: IGNORED_INCOMING_TRACE_ROUTES,
     ignoreOutgoingUrls: IGNORED_OUTGOING_TRACE_ROUTES,
