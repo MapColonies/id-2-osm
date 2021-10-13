c513a117b7f5a7554fdbcc900eee20b8d7e50eeb
Daniel Hermon <danielhermonsync@gmail.com>
2021-10-13 10:42:25 +0300

    feat: add pg connection name (#105)
    * feat: add pg connection name
    
    * fix: fixed application name in pg connection
    
    * build(deps): upgrade better-sqlite3 version

--- src/common/db/connection.ts
+++ src/common/db/connection.ts
@@ -1,9 +1,12 @@
+import { hostname } from 'os';
 import { readFileSync } from 'fs';
 import { Connection, ConnectionOptions, createConnection } from 'typeorm';
 import { DbConfig } from '../interfaces';
 
 export const createConnectionOptions = (dbConfig: DbConfig): ConnectionOptions => {
   const { enableSslAuth, sslPaths, ...connectionOptions } = dbConfig;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  connectionOptions.extra = { application_name: `${hostname()}-${process.env.NODE_ENV ?? 'unknown_env'}` };
   if (enableSslAuth && connectionOptions.type === 'postgres') {
     connectionOptions.password = undefined;
     connectionOptions.ssl = { key: readFileSync(sslPaths.key), cert: readFileSync(sslPaths.cert), ca: readFileSync(sslPaths.ca) };
