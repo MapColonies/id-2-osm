3abd1383f4dbc1d787413cafdf3339aa0ef02fda
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2022-06-09 10:21:56 +0300

    fix: fixed broken typeorm datasource file

--- dataSource.ts
+++ dataSource.ts
@@ -1,15 +1,13 @@
+import { DataSource } from 'typeorm';
 import config from 'config';
 import { createConnectionOptions } from './src/common/db/connection';
 import { DbConfig } from './src/common/interfaces';
 
 const connectionOptions = config.get<DbConfig>('db');
 
-module.exports = {
+export const appDataSource = new DataSource({
   ...createConnectionOptions(connectionOptions),
   entities: ['src/entity/models/*.ts'],
   migrationsTableName: 'custom_migration_table',
   migrations: ['db/migration/*.ts'],
-  cli: {
-    migrationsDir: 'db/migration',
-  },
-};
+});
