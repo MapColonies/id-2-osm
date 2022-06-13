79e3e4ed916648f2414001dfb0fe8ec150b0f4bd
Ofer <12687466+CptSchnitz@users.noreply.github.com>
2022-06-13 15:18:24 +0300

    refactor: upgraded telemetry (#152)

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -5,7 +5,7 @@ export const DEFAULT_SERVER_PORT = 80;
 export const DB_CONNECTION_TIMEOUT = 5000;
 
 export const IGNORED_OUTGOING_TRACE_ROUTES = [/^.*\/v1\/metrics.*$/];
-export const IGNORED_INCOMING_TRACE_ROUTES = [/^.*\/docs.*$/];
+export const IGNORED_INCOMING_TRACE_ROUTES = [/^.*\/docs.*$/, /^.*\/metrics.*/];
 
 /* eslint-disable @typescript-eslint/naming-convention */
 export const SERVICES: Record<string, symbol> = {
