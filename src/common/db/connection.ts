d2a3de6cfae768f3d157a49224471a53c9e78573
Niv Greenstein <greenstein9@gmail.com>
2025-01-12 19:18:00 +0200

    feat: added getDbHealthCheckFunction

--- src/common/db/connection.ts
+++ src/common/db/connection.ts
@@ -1,8 +1,12 @@
 import { hostname } from 'os';
 import { readFileSync } from 'fs';
+import { HealthCheck } from '@godaddy/terminus';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { Entity } from '../../entity/models/entity';
 import { DbConfig } from '../interfaces';
+import { promiseTimeout } from '../utils/promiseTimeout';
+
+const DB_TIMEOUT = 5000;
 
 export const createConnectionOptions = (dbConfig: DbConfig): DataSourceOptions => {
   const { enableSslAuth, sslPaths, ...dataSourceOptions } = dbConfig;
@@ -19,4 +23,13 @@ export const initConnection = async (dbConfig: DbConfig): Promise<DataSource> =>
   const dataSource = new DataSource(createConnectionOptions(dbConfig));
   await dataSource.initialize();
   return dataSource;
+};
+
+export const getDbHealthCheckFunction = (connection: DataSource): HealthCheck => {
+  return async (): Promise<void> => {
+    const check = connection.query('SELECT 1').then(() => {
+      return;
+    });
+    return promiseTimeout<void>(DB_TIMEOUT, check);
+  };
 };
