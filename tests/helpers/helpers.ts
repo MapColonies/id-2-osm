3d9e28baa78aa88ef2801251cf5f31a3d6c92ee9
Niv Greenstein <greenstein9@gmail.com>
2025-01-12 19:08:29 +0200

    fix: updated faker functions

--- tests/helpers/helpers.ts
+++ tests/helpers/helpers.ts
@@ -2,9 +2,9 @@ import { faker } from '@faker-js/faker';
 import { IEntity } from '../../src/entity/models/entity';
 
 export const createOsmId = (): number => {
-  return faker.datatype.number({ min: 1, max: 2147483647 });
+  return faker.number.int({ min: 1, max: 2147483647 });
 };
 
 export const createFakeEntity = (): IEntity => {
-  return { externalId: faker.datatype.uuid(), osmId: createOsmId() };
+  return { externalId: faker.string.uuid(), osmId: createOsmId() };
 };
