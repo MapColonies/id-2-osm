6d1f173a6685d9870d28bba099891dda467663ae
Niv Greenstein <greenstein9@gmail.com>
2025-01-12 19:09:17 +0200

    fix: added container parameter insted of getting global container

--- tests/integration/entity/helpers/db.ts
+++ tests/integration/entity/helpers/db.ts
@@ -1,9 +1,9 @@
-import { container } from 'tsyringe';
 import { DataSource } from 'typeorm';
+import { DependencyContainer } from 'tsyringe';
 import { Entity, IEntity } from '../../../../src/entity/models/entity';
 import { createFakeEntity } from '../../../helpers/helpers';
 
-export const createDbEntity = async (): Promise<IEntity> => {
+export const createDbEntity = async (container: DependencyContainer): Promise<IEntity> => {
   const conn = container.resolve(DataSource);
   const repo = conn.getRepository(Entity);
   const entity = repo.create(createFakeEntity());
