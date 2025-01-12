0a8c6925b55a6fb5fa8ba125dd8b5ba398303b8e
Niv Greenstein <greenstein9@gmail.com>
2025-01-12 19:10:56 +0200

    refactor: delete getApp and getMockedRepoApp

--- tests/integration/entity/helpers/requestSender.ts
+++ tests/integration/entity/helpers/requestSender.ts
@@ -1,20 +1,6 @@
 import * as supertest from 'supertest';
 import { Application } from 'express';
 
-import { container } from 'tsyringe';
-import { ServerBuilder } from '../../../../src/serverBuilder';
-
-export function getApp(): Application {
-  const builder = container.resolve<ServerBuilder>(ServerBuilder);
-  return builder.build();
-}
-
-export function getMockedRepoApp(repo: unknown): Application {
-  container.register('EntityRepository', { useValue: repo });
-  const builder = container.resolve<ServerBuilder>(ServerBuilder);
-  return builder.build();
-}
-
 export async function createEntity(app: Application, entity: { osmId?: unknown; externalId?: unknown }): Promise<supertest.Response> {
   return supertest.agent(app).post('/entity').set('Content-Type', 'application/json').send(entity);
 }
