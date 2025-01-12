b76f61bafb499099eb66df3feb2f3451ba069fba
Niv Greenstein <greenstein9@gmail.com>
2025-01-12 19:00:10 +0200

    refactor: updated containerConfig

--- src/containerConfig.ts
+++ src/containerConfig.ts
@@ -1,60 +1,57 @@
-import { container } from 'tsyringe';
-import config from 'config';
-import { DataSource } from 'typeorm';
-import jsLogger, { LoggerOptions } from '@map-colonies/js-logger';
-import { getOtelMixin, Metrics } from '@map-colonies/telemetry';
-import { metrics } from '@opentelemetry/api-metrics';
-import { HealthCheck } from '@godaddy/terminus';
+import { getOtelMixin } from '@map-colonies/telemetry';
 import { trace } from '@opentelemetry/api';
-import { SERVICES, SERVICE_NAME, DB_CONNECTION_TIMEOUT } from './common/constants';
-import { Entity } from './entity/models/entity';
-import { promiseTimeout } from './common/utils/promiseTimeout';
-import { DbConfig } from './common/interfaces';
+import { Registry } from 'prom-client';
+import { DependencyContainer } from 'tsyringe/dist/typings/types';
+import jsLogger from '@map-colonies/js-logger';
+import { InjectionObject, registerDependencies } from '@common/dependencyRegistration';
+import { ON_SIGNAL, SERVICES, SERVICE_NAME } from '@common/constants';
+import { getTracing } from '@common/tracing';
+import { DataSource } from 'typeorm';
+import { ENTITY_ROUTER_SYMBOL, entityRouterFactory } from './entity/routes/entityRouter';
+import { getConfig } from './common/config';
 import { initConnection } from './common/db/connection';
-import { tracing } from './common/tracing';
+import { ENTITY_REPOSITORY_SYMBOL } from './entity/models/entityManager';
+import { Entity } from './entity/models/entity';
+import { DbConfig } from './common/interfaces';
 
-const healthCheck = (connection: DataSource): HealthCheck => {
-  return async (): Promise<void> => {
-    const check = connection.query('SELECT 1').then(() => {
-      return;
-    });
-    return promiseTimeout<void>(DB_CONNECTION_TIMEOUT, check);
-  };
-};
-
-const beforeShutdown = (connection: DataSource): (() => Promise<void>) => {
-  return async (): Promise<void> => {
-    await connection.close();
-  };
-};
-
-async function registerExternalValues(): Promise<void> {
-  const loggerConfig = config.get<LoggerOptions>('telemetry.logger');
-  const logger = jsLogger({ ...loggerConfig, mixin: getOtelMixin() });
-
-  container.register(SERVICES.CONFIG, { useValue: config });
-  container.register(SERVICES.LOGGER, { useValue: logger });
-
-  const connectionOptions = config.get<DbConfig>('db');
-  const connection = await initConnection({ entities: ['entity/models/*.js'], ...connectionOptions });
-
-  container.register(SERVICES.HEALTHCHECK, { useValue: healthCheck(connection) });
-
-  container.register(DataSource, { useValue: connection });
-  container.register('EntityRepository', { useValue: connection.getRepository(Entity) });
-
-  const tracer = trace.getTracer(SERVICE_NAME);
-  container.register(SERVICES.TRACER, { useValue: tracer });
-
-  const otelMetrics = new Metrics();
-  otelMetrics.start();
-  container.register(SERVICES.METER, { useValue: metrics.getMeter('id-2-osm') });
-
-  container.register('onSignal', {
-    useValue: async (): Promise<void> => {
-      await Promise.all([tracing.stop(), otelMetrics.stop(), beforeShutdown(connection)]);
-    },
-  });
+export interface RegisterOptions {
+  override?: InjectionObject<unknown>[];
+  useChild?: boolean;
 }
 
-export { registerExternalValues };
+export const registerExternalValues = async (options?: RegisterOptions): Promise<DependencyContainer> => {
+  const configInstance = getConfig();
+
+  const loggerConfig = configInstance.get('telemetry.logger');
+
+  const logger = jsLogger({ ...loggerConfig, prettyPrint: loggerConfig.prettyPrint, mixin: getOtelMixin() });
+
+  const tracer = trace.getTracer(SERVICE_NAME);
+  const metricsRegistry = new Registry();
+  configInstance.initializeMetrics(metricsRegistry);
+
+  const connectionOptions = configInstance.get('db') as DbConfig;
+  const connection = await initConnection(connectionOptions);
+
+  const dependencies: InjectionObject<unknown>[] = [
+    { token: SERVICES.CONFIG, provider: { useValue: configInstance } },
+    { token: SERVICES.LOGGER, provider: { useValue: logger } },
+    { token: SERVICES.TRACER, provider: { useValue: tracer } },
+    { token: SERVICES.METRICS, provider: { useValue: metricsRegistry } },
+    { token: ENTITY_ROUTER_SYMBOL, provider: { useFactory: entityRouterFactory } },
+    {
+      token: ON_SIGNAL,
+      provider: {
+        useValue: {
+          useValue: async (): Promise<void> => {
+            await Promise.all([getTracing().stop(), connection.destroy()]);
+          },
+        },
+      },
+    },
+    { token: DataSource, provider: { useValue: connection } },
+    { token: ENTITY_REPOSITORY_SYMBOL, provider: { useValue: connection.getRepository(Entity) } },
+  ];
+
+  return Promise.resolve(registerDependencies(dependencies, options?.override, options?.useChild));
+};
