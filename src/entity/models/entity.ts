15224c4ea8026fb87485642a55f9640067f8be17
Niv Greenstein <greenstein9@gmail.com>
2025-01-12 18:57:26 +0200

    fix: added missing type and transformer

--- src/entity/models/entity.ts
+++ src/entity/models/entity.ts
@@ -7,10 +7,17 @@ export interface IEntity {
 
 @OrmEntity()
 export class Entity implements IEntity {
-  @PrimaryColumn({ name: 'external_id', length: 68 })
+  @PrimaryColumn({ type: 'varchar', name: 'external_id', length: 68 })
   public externalId!: string;
 
   @Index()
-  @Column({ type: 'bigint', name: 'osm_id' })
+  @Column({
+    type: 'bigint',
+    name: 'osm_id',
+    transformer: {
+      to: (value: number) => value,
+      from: (value: number) => parseInt(value.toString()),
+    },
+  })
   public osmId!: number;
 }
