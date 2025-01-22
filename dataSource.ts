5b5e9e3414c65fc518c323ddf11e279e944a02bf
Niv Greenstein <greenstein9@gmail.com>
2025-01-22 15:21:18 +0200

    fix: changed top-level dataSource connection  to custom function using new mapcolonies config

--- dataSource.ts
+++ dataSource.ts
@@ -1,13 +1,22 @@
 import { DataSource } from 'typeorm';
-import config from 'config';
 import { createConnectionOptions } from './src/common/db/connection';
 import { DbConfig } from './src/common/interfaces';
+import { getConfig, initConfig } from './src/common/config';
 
-const connectionOptions = config.get<DbConfig>('db');
+const dataSourceFactory = async (): Promise<DataSource> => {
+  await initConfig(true);
+  const config = getConfig();
 
-export const appDataSource = new DataSource({
-  ...createConnectionOptions(connectionOptions),
-  entities: ['src/entity/models/*.ts'],
-  migrationsTableName: 'custom_migration_table',
-  migrations: ['db/migration/*.ts'],
-});
+  const connectionOptions = config.get('db') as DbConfig;
+
+  const appDataSource = new DataSource({
+    ...createConnectionOptions(connectionOptions),
+    entities: ['src/entity/models/*.ts'],
+    migrationsTableName: 'custom_migration_table',
+    migrations: ['db/migration/*.ts'],
+  });
+
+  return appDataSource;
+};
+
+export default dataSourceFactory();
